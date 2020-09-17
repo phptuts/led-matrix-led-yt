5b9ccea998e0bf566ced857675ef3299665c4a1a
Noah Glaser <glaserpower@gmail.com>
2020-09-16 21:11:32 -0700

    led matrix

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
   import _ from "lodash";
-
+  let port;
+  let writer;
+  let status = "disconnected";
   let leds = _.range(0, 8).map(() => _.range(0, 8).map(() => false));
-  console.log(leds);
+
+  const textEncoder = new TextEncoder();
 
   function onToggleLed(row, column) {
     leds[row][column] = !leds[row][column];
@@ -15,6 +18,33 @@
         return acc + value.map((l) => (l ? "1" : "0")).join(":") + ":";
       }, "")
       .slice(0, -1) + "@";
+
+  async function connectUsb() {
+    if (!navigator.serial) {
+      alert("Turn on experimential web features to connect to the Arduino.");
+      return;
+    }
+
+    // if it's connected we want to return
+    if (status === "connected") {
+      return;
+    }
+
+    try {
+      port = await navigator.serial.requestPort({
+        filters: [{ usbVendorId: 0x2341 }],
+      });
+      await port.open({ baudrate: 115200 });
+      writer = port.writable.getWriter();
+      status = "connected";
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  $: if (status == "connected" && arduinoMessage != "") {
+    writer.write(textEncoder.encode(arduinoMessage));
+  }
 </script>
 
 <style>
@@ -51,9 +81,38 @@
     padding: 5px;
     margin: 10px auto;
   }
+  #usb-connection-info {
+    width: 500px;
+    margin: 5px auto;
+    text-align: center;
+  }
+  #usb-connection-info h3 {
+    letter-spacing: 2px;
+  }
+  #usb-connection-info button {
+    border: none;
+    padding: 10px;
+    outline: none;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  #usb-connection-info button:disabled {
+    cursor: no-drop;
+  }
+  button:active,
+  button:focus {
+    outline: none;
+  }
 </style>
 
 <h1>Led Matrix Arduino</h1>
+
+<section id="usb-connection-info">
+  <button
+    disabled={status === 'connected'}
+    on:click={connectUsb}>Connect</button>
+  <h3>Status: {status}</h3>
+</section>
 
 <main>
   {#each leds as row, rowIndex}
